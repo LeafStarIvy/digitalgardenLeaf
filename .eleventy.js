c72ee41a0c0bcf66146fe5f9bead326953c596c8
LeafStarIvy <k.d.mooibroek@gmail.com>
2025-04-21 17:18:35 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -94,6 +94,10 @@ function getAnchorAttributes(filePath, linkTitle) {
 const tagRegex = /(^|\s|\>)(#[^\s!@#$%^&*()=+\.,\[{\]};:'"?><]+)(?!([^<]*>))/g;
 
 module.exports = function (eleventyConfig) {
+   eleventyConfig.addPassthroughCopy("downloader.html");
+  // If you used a public folder:
+  // eleventyConfig.addPassthroughCopy("public");
+};
   eleventyConfig.setLiquidOptions({
     dynamicPartials: true,
   });
