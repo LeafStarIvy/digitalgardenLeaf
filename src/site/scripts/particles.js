fa80c183f6eae4cb842eb8205523155fd5332c98
LeafStarIvy <k.d.mooibroek@gmail.com>
2025-04-24 19:57:50 +0200

    Update particles.js

--- src/site/scripts/particles.js
+++ src/site/scripts/particles.js
@@ -1,7 +1,7 @@
-// src/site/scripts/particles.js
 (() => {
   const body = document.body;
   document.addEventListener('pointermove', e => {
+    // moves ±10px at cursor extremes
     const x = (e.clientX / window.innerWidth  - 0.5) * 20;
     const y = (e.clientY / window.innerHeight - 0.5) * 20;
     body.style.setProperty('--particle-offset-x', `${x}px`);
